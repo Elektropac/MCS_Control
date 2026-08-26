3029e0ed5fee6eedbf03cf29c7e423b58d7a7c65
ElektroPac <43667224+Elektropac@users.noreply.github.com>
2026-08-26 13:31:23 +0200

    Remove unused pub/sub components (using direct function calls instead)
    Removed:
    - lib/mcs_bus/ (message bus)
    - lib/mcs_api/ (pub/sub wrapper)
    - src/apps/demo_sensor/
    - src/apps/_template/
    - src/apps/app_registry/

--- src/apps/_template/template_app.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <ArduinoJson.h>
-
-void template_app_init(const JsonObject& config);
-void template_app_task(void* param);
--- src/apps/demo_sensor/demo_sensor.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <ArduinoJson.h>
-
-void demo_sensor_init(const JsonObject& config);
-void demo_sensor_task(void* param);
